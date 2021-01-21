1d5bf63ac5aaaa66c1946642f69b7e0bb16b3230
julicardozo <jcardozo@fi.uba.ar>
2021-01-21 17:08:24 -0300

    se puede poner el texto que se quiere comentar o postear

--- facebook_pruebas.py
+++ facebook_pruebas.py
@@ -2,30 +2,40 @@ import facebook
 import json
 import time
 
+def login(token):
+    graph = facebook.GraphAPI(access_token=token, version="2.8")
+    app_id = "404375060791037"
+    canvas_url = "https://domain.com/that-handles-auth-response/"
+    perms = ["manage_pages","publish_pages"]
+    fb_login_url = graph.get_auth_url(app_id, canvas_url, perms)
+    print(fb_login_url)
 
-def mostrar_comentario():
-    graph = facebook.GraphAPI(access_token="EAAFvxufavv0BAIH4Hol4KiZBGfFe8EYgyP91J76vKLXRLArhQmCwLMd982r3D8tXryqEpJ1vFbGVfoZA8bsCo58KyYve5IZCP5RXbBqudYtP6xxZAHzHryNtRtiFJxHPgJXNZC7L7XUAp07LELolox1ZBA6NT5fROuUXP7odMfgrTkZCZAnULi4uz7CkbnsxJaR0dH5X1aHGpqsS5l0A9yok", version="2.8")
+
+def mostrar_comentario(token):
+    graph = facebook.GraphAPI(access_token=token, version="2.8")
     comments = graph.get_connections(id='105670634828356_107086578020095', connection_name='comments')
     print(comments)
 
-def comentar():
-    graph = facebook.GraphAPI(access_token="EAAFvxufavv0BAIH4Hol4KiZBGfFe8EYgyP91J76vKLXRLArhQmCwLMd982r3D8tXryqEpJ1vFbGVfoZA8bsCo58KyYve5IZCP5RXbBqudYtP6xxZAHzHryNtRtiFJxHPgJXNZC7L7XUAp07LELolox1ZBA6NT5fROuUXP7odMfgrTkZCZAnULi4uz7CkbnsxJaR0dH5X1aHGpqsS5l0A9yok", version="2.8")
-    comment = graph.put_object(parent_object='105670634828356_108396174555802', connection_name='comments',message='Soy Juli comentando')
+def comentar(token,mensaje):
+    graph = facebook.GraphAPI(access_token=token, version="2.8")
+    comment = graph.put_object(parent_object='106233258085979_107833407925964', connection_name='comments',message=mensaje)
     print(comment) 
 
-def likear():
-    graph = facebook.GraphAPI(access_token="EAAFvxufavv0BAIH4Hol4KiZBGfFe8EYgyP91J76vKLXRLArhQmCwLMd982r3D8tXryqEpJ1vFbGVfoZA8bsCo58KyYve5IZCP5RXbBqudYtP6xxZAHzHryNtRtiFJxHPgJXNZC7L7XUAp07LELolox1ZBA6NT5fROuUXP7odMfgrTkZCZAnULi4uz7CkbnsxJaR0dH5X1aHGpqsS5l0A9yok", version="2.8")
+def likear(token):
+    graph = facebook.GraphAPI(access_token=token, version="2.8")
     like = graph.put_like(object_id='105670634828356_108396174555802')
     print(like)
     
-def posteo():
-    graph = facebook.GraphAPI(access_token="EAAFvxufavv0BAIH4Hol4KiZBGfFe8EYgyP91J76vKLXRLArhQmCwLMd982r3D8tXryqEpJ1vFbGVfoZA8bsCo58KyYve5IZCP5RXbBqudYtP6xxZAHzHryNtRtiFJxHPgJXNZC7L7XUAp07LELolox1ZBA6NT5fROuUXP7odMfgrTkZCZAnULi4uz7CkbnsxJaR0dH5X1aHGpqsS5l0A9yok", version="2.8")
-    post=graph.put_object(parent_object='me', connection_name='feed',message='Holaa soy Juli posteando algo.')
+def posteo(token,mensaje):
+    graph = facebook.GraphAPI(access_token=token, version="2.8")
+    post=graph.put_object(parent_object='me', connection_name='feed',message=mensaje)
     print(post)
 
-def postear_foto():    
-    graph = facebook.GraphAPI(access_token="EAAFvxufavv0BAIH4Hol4KiZBGfFe8EYgyP91J76vKLXRLArhQmCwLMd982r3D8tXryqEpJ1vFbGVfoZA8bsCo58KyYve5IZCP5RXbBqudYtP6xxZAHzHryNtRtiFJxHPgJXNZC7L7XUAp07LELolox1ZBA6NT5fROuUXP7odMfgrTkZCZAnULi4uz7CkbnsxJaR0dH5X1aHGpqsS5l0A9yok", version="2.8")
+def postear_foto(token):    
+    graph = facebook.GraphAPI(access_token=token, version="2.8")
     foto = graph.put_photo(image=open('foto.jpeg', 'rb'),message='FOTON!')
     print(foto)
+
+
 
 
