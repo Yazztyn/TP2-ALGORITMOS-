6d6b4681d79330bf2a82a276c5842ba675ceac8c
julicardozo <jcardozo@fi.uba.ar>
2021-01-29 13:54:22 -0300

    Al probarlo hay que cambiar el token por uno activo al momento de usarlo

--- menu_prueba.py
+++ menu_prueba.py
@@ -1,26 +1,22 @@
 import facebook_pruebas
 import facebook
-
+from PIL import Image
 def menu():
 
-    token = "EAAFvxufavv0BAOh41PazAEmjHaiZAqzEhw8TKVZBZBoBHUc5OvJuK57rkMdlah4VZAui3ZBpsZCLZAjCfCSMvy28wMTagAsoDZBurS9LP6ZAkAjh9AfGovrH1ZCR327y88yqF7Sdi0Sj4S8nZC034pqcukiWSOFbzQArb5MEqRldq57BQPmBvsPjtGw6GD66PgCB4cfOU63vEEA1xygsw08wrnZB"
+    token = "EAAFvxufavv0BAI200L66QecH6kS2U4iIZAALGWOKX2UZCXZB2ywwljavScANopFqLBnU81QhAjCrdUZCWYjdtuafAKibmZBDnwnpZB4kw6mIkIayx4zPIs0cAZBMXka4oODMbM5ZAHWrA77L3xhA9Qo3AkVvI3Fbp1Bz1ZAHhtmZBYI4tL5ZB5NRp6uNYxOd5aScUdnGO5ZAXRy3zd93tImLJdHc"
    
-    condicion = True
+    continuar = True
     
-    while condicion:
+    while continuar:
         # Desplegamos el menú   
         print("""
-            1) Buscar usarios
-            2) Dar "likes" a posteos
-            3) Leer posteos
-            4) Subir posteos y fotos
-            5) Actualizar un posteo
-            6) Listar a los seguidores/amigos
-            7) Seguir a un usuario/o solicitar amistad
-            8) Enviar mensaje a un usuario
-            9) Actualizar datos del perfil
-            10) Comentar un posteo
-            11) Postear una foto.
+            1) Dar "like" a posteo.
+            2) Mostrar posteos.
+            3) Subir posteo.
+            4) Listar a los seguidores/amigos.
+            5) Enviar mensaje a un usuario.
+            6) Comentar un posteo.
+            7) Postear una foto.
             """)
 
         # Leemos lo que ingresó el usuario
@@ -28,41 +24,85 @@ def menu():
 
         # Según lo que haya ingresado, le damos lo que pidió
         if eligio== "1":
-            print("Usted eligió: Buscar usuarios")
-            condicion = False
+            
+            print("Usted eligió: Dar likes a posteo.")
+            post = facebook_pruebas.mostrar_posts(token) 
+            posteo = post["posts"]["data"]
+            
+            for diccionario in range(len(posteo) - 3):
+                print(str(diccionario) + ":" + posteo[diccionario]["message"])
+                
+            eleccion_likear = int(input("\nIngrese el número de posteo que desea comentar: ")) 
+            id = posteo[eleccion_likear]["id"]
+            facebook_pruebas.likear(token,id)
+            
+            continuar = False
+        
         elif eligio== "2":
-            print("Usted eligió: Dar likes a posteos")
-            facebook_pruebas.likear(token)
-            condicion = False
+            
+            print("Usted eligió: Mostrar posteos.")
+            post = facebook_pruebas.mostrar_posts(token)
+            
+            posteo = post["posts"]["data"]
+            
+            for diccionario in range(len(posteo) - 3):
+                fecha = posteo[diccionario]["created_time"]
+                texto = posteo[diccionario]["message"]
+                print(str(diccionario) + "\t" + texto + "\t\t" + "--Fecha: " + fecha)
+            
+            continuar = False
+
         elif eligio== "3":
-            print("Usted eligió: Leer posteos")
-            condicion = False
-        elif eligio== "4":
-            print("Usted eligió: Subir posteo")
+
+            print("Usted eligió: Subir posteo.")
             mensaje = input("Ingrese e texto que quiere postear: ")
             facebook_pruebas.posteo(token,mensaje)
-            condicion = False
-        elif eligio == "5":
-            print("Usted eligió: Actualizar un posteo")
-            condicion = False
-        elif eligio == "6":
+            
+            continuar = False
+        
+        elif eligio == "4":
+            
             print("Usted eligió: Listar a los seguidores/amigos")
-            condicion = False
-        elif eligio == "7":
-            print("Usted eligió: Seguir a un usuario/o solicitar amistad")
-            condicion = False
-        elif eligio == "8":
+            continuar = False
+            
+        elif eligio == "5":
+
             print("Usted eligió: Enviar un mensaje a un usuario")
-            condicion = False
-        elif eligio == "9":
-            print("Usted eligió: Actualizar datos del perfil")
-            condicion = False
-        elif eligio == "10":
+            
+            continuar = False
+
+        elif eligio == "6":
+
             print("Usted eligió: Comentar un posteo")
+            post = facebook_pruebas.mostrar_posts(token) 
+            posteo = post["posts"]["data"]
+            
+            for diccionario in range(len(posteo) - 3):
+                print(str(diccionario) + ":" + posteo[diccionario]["message"])
+                
+            eleccion_comentar = int(input("\nIngrese el número de posteo que desea comentar: ")) 
+            id = posteo[eleccion_comentar]["id"]
             mensaje = input("Ingrese el texto que quiere comentar: ")
-            facebook_pruebas.comentar(token,mensaje)
-            condicion = False 
-        elif eligio == "11":
-            facebook_pruebas.postear_foto(token)
+            facebook_pruebas.comentar(token,mensaje,id)
+            
+            continuar = False 
+
+        elif eligio == "7":
+            
+            print("Usted eligió; Postear una foto.")
+            
+            imagen = input("Ingrese el nombre de la imagen que desea subir (no olvide añadir .jpg o .png): ")
+
+            mensaje = input("Ingrese el texto que quiere publicar junto con la foto: ")
+
+            facebook_pruebas.postear_foto(token,mensaje,imagen)
+
+            img = Image.open(imagen,'r')
+
+            img.show()
+
+        
+            continuar = False
+
         else:
-            print("Error: El numero ingresado no es el correcto. Vuelva a intentarlo")
+            print("Error: El ingreso no es el correcto. Vuelva a intentarlo")
