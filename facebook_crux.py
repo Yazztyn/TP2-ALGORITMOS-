694dee868dcaf300a2b74bce2a262833ee87740b
julicardozo <jcardozo@fi.uba.ar>
2021-02-18 17:03:18 -0300

    Le agregue los pre y post que hiciste vos

--- facebook_crux.py
+++ facebook_crux.py
@@ -4,35 +4,55 @@ import time
 from PIL import Image
 
 def mostrar_posts(token):
+    """ Pre: Ingresa el token por parámetro
+        Post: Devuelve el posteo
+    """
     graph = facebook.GraphAPI(access_token=token, version="2.8")
     posteos = graph.get_object(id = "105670634828356",fields = "posts")
     return posteos
 
 def mostrar_comentario(token):
+    """ Pre: Ingresa el token por parámetro
+        Post: Imprime los comentarios del posteo elegido
+    """
     graph = facebook.GraphAPI(access_token=token, version="2.8")
     comments = graph.get_connections(id='105670634828356_107086578020095', connection_name='comments')
     print(comments)
 
 def comentar(token,mensaje,id):
+    """ Pre: Ingresa el token,el mensaje que desea el usuario y el id del post por parámetro
+        Post: Comenta el post con lo que el usuario ingresó
+    """
     graph = facebook.GraphAPI(access_token=token, version="2.8")
     comment = graph.put_object(parent_object= id, connection_name='comments',message=mensaje)
     print(comment) 
 
 def likear(token,id):
+    """ Pre: Ingresa el token y el id del post por parámetro
+        Post: Likea el post 
+    """
     graph = facebook.GraphAPI(access_token=token, version="2.8")
     graph.put_like(object_id=id)
-    
-    
+      
 def posteo(token,mensaje):
+     """ Pre: Ingresa el token y el mensaje que acompaña el mismo por parámetro
+        Post: Publica un estado en Facebook
+    """
     graph = facebook.GraphAPI(access_token=token, version="2.8")
     post=graph.put_object(parent_object='me', connection_name='feed',message=mensaje)
     print(post)
 
-def postear_foto(token,mensaje,imagen):    
+def postear_foto(token,mensaje,imagen):  
+    """ Pre: Ingresa el token, un mensaje y una imagen por parámetro
+        Post: Publica una foto con un mensaje en Facebook
+    """  
     graph = facebook.GraphAPI(access_token=token, version="2.8")
     graph.put_photo(image=open(imagen, 'rb'),message=mensaje)
     
 def obtener_id(token):
+    """ Pre: Ingresa el token por parámetro
+        Post: Devuelve el ID de la persona misma
+    """
     graph = facebook.GraphAPI(access_token=token, version="2.8")
     id_propio = graph.get_object(id = 'me', fields = 'id')
     return id_propio
